cfaf6f27e767004c30e8ce8da2a8e22f0a77f7a2
wtRinaldi <williamtrinaldi@gmail.com>
2018-10-03 11:14:25 -0400

    fix markers

--- js/index.js
+++ js/index.js
@@ -82,42 +82,5 @@ app.controller('MainCtrl', ['$scope', 'date', 'earthquakeData', function($scope,
       map: map,
       title: $scope.earthquake.magnitude.toString()
     })
-  }
-
-
-
-
-  
+  }  
 }]);
- 
-
-/*    $scope.markers = [];
-    
-    var infoWindow = new google.maps.InfoWindow();
-    
-    var createMarker = function (info){
-        
-        var marker = new google.maps.Marker({
-            map: $scope.map,
-            position: new google.maps.LatLng(info.lat, info.long),
-            title: info.city
-        });
-        marker.content = '<div class="infoWindowContent">' + info.desc + '</div>';
-        
-        google.maps.event.addListener(marker, 'click', function(){
-            infoWindow.setContent('<h2>' + marker.title + '</h2>' + marker.content);
-            infoWindow.open($scope.map, marker);
-        });
-        
-        $scope.markers.push(marker);
-        
-    }  
-    
-    for (i = 0; i < cities.length; i++){
-        createMarker(cities[i]);
-    }
-
-    $scope.openInfoWindow = function(e, selectedMarker){
-        e.preventDefault();
-        google.maps.event.trigger(selectedMarker, 'click');
-    }*/
